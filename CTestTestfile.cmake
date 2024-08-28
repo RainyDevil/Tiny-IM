7a1a78bb71bae671bc7e01f91cfcf600196faf5c
YKChen <2371208450@qq.com>
2024-08-28 11:03:28 +0800

    config.json

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/test/chatroom
-# Build directory: /root/test/chatroom
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
